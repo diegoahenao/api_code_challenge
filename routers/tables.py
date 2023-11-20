59d5899927b094b67273c925ab3f10f1825c9e4b
diegoahenao <ggodie85@gmail.com>
2023-11-20 08:10:26 -0500

    update models and fix create_job method

--- routers/tables.py
+++ routers/tables.py
@@ -9,10 +9,17 @@ from routers.auth import get_current_user
 
 tables_router = APIRouter()
 
-@tables_router.post('/departments', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+@tables_router.post('/department', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
 def create_department_api(department: DepartmentsCreate) -> dict:
     db = sessionlocal()
     Tables(db).create_department(department)
+    return JSONResponse(status_code=201, content={"message": "Department registered"})
+
+@tables_router.post('/departments', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+def create_departments_api(departments: List[DepartmentsCreate]) -> dict:
+    for department in departments:
+        db = sessionlocal()
+        Tables(db).create_department(department)
     return JSONResponse(status_code=201, content={"message": "Departments registered"})
 
 @tables_router.get('/departments', tags=['tables'], response_model=List[DepartmentsCreate], status_code=200, dependencies=[Depends(get_current_user)])
@@ -21,10 +28,17 @@ def read_departments_api() -> List[DepartmentsCreate]:
     result = Tables(db).get_departments()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
-@tables_router.post('/jobs', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+@tables_router.post('/job', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
 def create_job_api(job: JobsCreate) -> dict:
     db = sessionlocal()
     Tables(db).create_job(job)
+    return JSONResponse(status_code=201, content={"message": "Job registered"})
+
+@tables_router.post('/jobs', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+def create_jobs_api(jobs: List[JobsCreate]) -> dict:
+    for job in jobs:
+        db = sessionlocal()
+        Tables(db).create_job(job)
     return JSONResponse(status_code=201, content={"message": "Jobs registered"})
 
 @tables_router.get('/jobs', tags=['tables'], response_model=List[JobsCreate], status_code=200, dependencies=[Depends(get_current_user)])
@@ -33,10 +47,17 @@ def read_jobs_api() -> List[JobsCreate]:
     result = Tables(db).get_jobs()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
-@tables_router.post('/hired_employees', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+@tables_router.post('/hired_employee', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
 def create_hired_employee_api(hired_employee: HiredEmployeesCreate) -> dict:
     db = sessionlocal()
     Tables(db).create_hired_employee(hired_employee)
+    return JSONResponse(status_code=201, content={"message": "Hired employees registered"})
+
+@tables_router.post('/hired_employees', tags=['tables'], response_model=dict, status_code=201, dependencies=[Depends(get_current_user)])
+def create_hired_employee_api(hired_employees: List[HiredEmployeesCreate]) -> dict:
+    for hired_employee in hired_employees:
+        db = sessionlocal()
+        Tables(db).create_hired_employee(hired_employee)
     return JSONResponse(status_code=201, content={"message": "Hired employees registered"})
 
 @tables_router.get('/hired_employees', tags=['tables'], response_model=List[HiredEmployeesCreate], status_code=200, dependencies=[Depends(get_current_user)])
