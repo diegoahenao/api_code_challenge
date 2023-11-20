069a299e2276128a07f93ec4cd7eea513aa74f0b
diegoahenao <ggodie85@gmail.com>
2023-11-19 22:45:12 -0500

    create schemas and services get and create

---
+++ schemas/schema_tables.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+
+class HiredEmployeesCreate(BaseModel):
+    id: int = Field(gt=0)
+    name: str = Field(min_length=2)
+    datetime: str
+    department_id: int = Field(gt=0)
+    job_id: int = Field(gt=0)
+
+class DepartmentsCreate(BaseModel):
+    id: int = Field(gt=0)
+    department: str = Field(min_length=2)
+
+class JobsCreate(BaseModel):
+    id: int = Field(gt=0)
+    job: str = Field(min_length=2)
